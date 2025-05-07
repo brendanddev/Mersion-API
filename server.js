3871caef17fc5ee7fee4cfffacbb23eb6ec525f0
Brendan Dileo <dileob23@gmail.com>
2025-05-07 18:48:44 -0400

    Basic Server Implementation

--- server.js
+++ server.js
@@ -2,3 +2,24 @@
 // server.js
 // Brendan Dileo, May 2025
 
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const sqlite3 = require('better-sqlite3');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Test Route
+app.get('/', (req, res) => {
+    console.log('Request');
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`);
+}); 

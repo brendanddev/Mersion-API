3a5f4e0ef7f4eafe0bebfe1816be17ebda0d96e0
Brendan Dileo <dileob23@gmail.com>
2025-05-29 18:43:50 -0400

    Setup basic server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});

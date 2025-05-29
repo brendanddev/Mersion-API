8114f912a823245503fd0b22c499a9bfc7b6a1d6
Brendan Dileo <dileob23@gmail.com>
2025-05-29 18:53:32 -0400

    Add logic for connecting to db, setting up comic schema, and logger

--- models/comicModel.js
+++ models/comicModel.js
@@ -1,1 +1,21 @@
 
+
+// comicModel.js
+// Defines schema and model for comics in the database
+
+const mongoose = require('mongoose');
+
+const comicSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: String,
+    publisher: String,
+    issue: Number,
+    volume: Number,
+    genre: String,
+    releaseDate: Date,
+    description: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Comic', comicSchema);

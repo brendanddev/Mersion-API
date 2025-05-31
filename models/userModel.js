b4e4270d696f13fcc40797692e55b16e3a4bd325
Brendan Dileo <dileob23@gmail.com>
2025-05-31 14:00:02 -0400

    Add password comparison logic

--- models/userModel.js
+++ models/userModel.js
@@ -20,3 +20,9 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
+
+userSchema.methods.comparePassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+module.exports = mongoose.model('User', userSchema);

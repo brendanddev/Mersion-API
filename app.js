e1b64963247721442a1b965815431f3b2dc7a178
Brendan Dileo <dileob23@gmail.com>
2025-05-31 14:19:29 -0400

    Update app

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const express = require('express');
 const cors = require('cors');
 const logger = require('./utils/logger');
 const comicRoutes = require('./routes/comicRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/comics', comicRoutes);
+app.use('/api/auth', authRoutes);
 
 app.use((err, req, res, next) => {
     logger.error(err.stack);

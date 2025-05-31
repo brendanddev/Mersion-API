f0ad24c4af926cc070a849cdaf4dd2fef678a6ac
Brendan Dileo <dileob23@gmail.com>
2025-05-31 13:39:08 -0400

    Create user model

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
     if (!token) return res.status(401).json({ error: 'Authorization denied. No token provided.' });
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_TOKEN);
         req.user = decoded;
         next();
     } catch (error) {

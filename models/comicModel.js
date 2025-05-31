bc98e803b415d9c2ad635b4006ea642ff4173a35
Brendan Dileo <dileob23@gmail.com>
2025-05-31 14:08:57 -0400

    Add comments

--- models/comicModel.js
+++ models/comicModel.js
@@ -4,12 +4,13 @@
 
 const mongoose = require('mongoose');
 
+// Comic schema definition
 const comicSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    author: String,
+    author: { type: String, required: true },
     publisher: String,
-    issue: Number,
-    volume: Number,
+    issue: { type: Number, required: true },
+    volume: { type: Number, required: true },
     genre: String,
     releaseDate: Date,
     description: String

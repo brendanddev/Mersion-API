66e0f91df197d7ae49ce51d66b56f5b28f222c88
Brendan Dileo <dileob23@gmail.com>
2025-05-31 14:01:16 -0400

    Setup auth route outline

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,3 +5,15 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const User = require('../models/userModel');
+
+const generateToken = (user) => {
+}
+
+// POST to register a new user
+router.post('/register', async (req, res) => {
+});
+
+// POST to login a user
+router.post('/login', async (req, res) => {
+});

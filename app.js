1e789df66158a0183a4d4ba01dd9c3bc8226ed04
Brendan Dileo <dileob23@gmail.com>
2025-06-05 18:58:44 -0400

    Remove logic, fresh start

--- app.js
+++ app.js
@@ -2,26 +2,3 @@
 // app.js
 // Main Express application setup
 // Brendan Dileo - May 2025
-
-const express = require('express');
-const cors = require('cors');
-const logger = require('./utils/logger');
-const comicRoutes = require('./routes/comicRoutes');
-const authRoutes = require('./routes/authRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/api/comics', comicRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
-
-app.use((err, req, res, next) => {
-    logger.error(err.stack);
-    res.status(500).json({ error: 'Uh oh! Something went wrong!' })
-});
-
-module.exports = app;
-
--- server.js
+++ server.js
@@ -2,19 +2,3 @@
 // server.js
 // Main entry point for the server application
 // Brendan Dileo - May 2025
-
-const dotenv = require('dotenv');
-const connectDB = require('./db/connect');
-const logger = require('./utils/logger');
-const app = require('./app');
-
-dotenv.config();
-const port = process.env.PORT || 4001;
-
-connectDB().then(() => {
-    app.listen(port, () => {
-        logger.log(`Server is running on http://localhost:${port}`);
-    });
-}).catch(err => {
-    logger.error('Failed to connect to DB: ' + err.message);
-});

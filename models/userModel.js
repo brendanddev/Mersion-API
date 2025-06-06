b6eb792649422f1da3ebf8860763d35fd0abfed5
Brendan Dileo <dileob23@gmail.com>
2025-06-06 19:35:48 -0400

    Setup refresh tokens and install cookie parser

--- models/userModel.js
+++ models/userModel.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    refreshToken: { type: String },
 });
 
 // Hash pswd before save

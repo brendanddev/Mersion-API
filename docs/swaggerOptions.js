39e2df96514542b8a6f6bf27fb40b7a6ebd7844d
Brendan Dileo <dileob23@gmail.com>
2025-06-06 08:15:34 -0400

    Update swagger options to load yaml file

--- docs/swaggerOptions.js
+++ docs/swaggerOptions.js
@@ -4,7 +4,7 @@
 // Brendan Dileo - June 2025
 
 const yaml = require('yamljs');
-const path = require('path');\
+const path = require('path');
 
-const swaggerDoc = yaml.load(path.join(__dirname, 'swagger.yaml'));
+const swaggerDoc = yaml.load(path.join(__dirname, 'swagger.yml'));
 module.exports = swaggerDoc;

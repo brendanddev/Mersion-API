2869fe5a7668f363e4428a14e53983c68d95853f
Brendan Dileo <dileob23@gmail.com>
2025-06-06 08:07:55 -0400

    Update swagger options

--- docs/swaggerOptions.js
+++ docs/swaggerOptions.js
@@ -3,4 +3,8 @@
 // Configures the swagger options for the api docs.
 // Brendan Dileo - June 2025
 
-const swaggerJsDoc = require('swagger-jsdoc');
+const yaml = require('yamljs');
+const path = require('path');\
+
+const swaggerDoc = yaml.load(path.join(__dirname, 'swagger.yaml'));
+module.exports = swaggerDoc;

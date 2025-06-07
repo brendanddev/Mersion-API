12912884bfe22dd75d76ee02d6a262bc67cc996e
Brendan Dileo <dileob23@gmail.com>
2025-06-07 13:24:06 -0400

    Implement access token and refresh token generation logic

--- utils/token.js
+++ utils/token.js
@@ -5,4 +5,27 @@
 
 const jwt = require('jsonwebtoken');
 
+// Generates jwt access token for a user
+const generateAccessToken = (user) => {
+    if (!user || !user._id) throw new Error('Invalid user object provided for token generation');
+    
+    return jwt.sign(
+        { id: user._id },
+        process.env.JWT_ACCESS_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRE }
+    );
+}
+
+// Generates jwt refresh token for a user
+const generateRefreshToken = (user) => {
+    if (!user || !user._id) throw new Error('Invalid user object provided for token generation');
+    
+    return jwt.sign(
+        { id: user._id },
+        process.env.JWT_REFRESH_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRE }
+    );
+}
+
+module.exports = { generateAccessToken, generateRefreshToken };
 

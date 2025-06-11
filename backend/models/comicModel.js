88198b7b9d48ece8d893863a7d299a7a6e19ad39
Brendan Dileo <dileob23@gmail.com>
2025-06-11 18:53:13 -0400

    Re-add logger and db connect

---
+++ backend/models/comicModel.js
@@ -0,0 +1,10 @@
+
+// comicModel.js
+
+const mongoose = require('mongoose');
+
+const comicSchema = mongoose.Schema({
+
+});
+
+module.exports = mongoose.model('Comic', comicSchema);
--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const logger = require('./utils/logger');
+
+const connectDB = require('./db/connect');
+const Comic = require('./models/comicModel');
 
 // Creates the app
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log(process.env.PORT);
+logger.log(process.env.PORT);
 
 // Basic middleware
 app.use(cors());
@@ -22,7 +26,12 @@ app.get('/', (req, res) => {
     res.send('Server up and running...');
 });
 
-// Starts the server and listens on port
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-})
+// Connect to db first then chain since connect returns a promise
+connectDB().then(() => {
+    // Starts the server and listens on port
+    app.listen(port, () => {
+        logger.log(`Server listening at http://localhost:${port}`);
+    });
+}).catch((error) => {
+    logger.error(`An error occurred: ${error.message}`);
+});

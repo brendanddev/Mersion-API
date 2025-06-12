56ea7bd2cc4765d7dcd7631cc4a8d6398e234257
Brendan Dileo <dileob23@gmail.com>
2025-06-12 19:36:44 -0400

    Update build and create cli app file

---
+++ cli/src/main/java/cli/App.java
@@ -0,0 +1,13 @@
+package cli;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("CLI APP");;
+    }
+
+
+
+    private static String printHeader() {
+        
+    }
+}

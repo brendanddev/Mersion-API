bb378e607499bf3f371d56f10bbfa7e4ada65012
Brendan Dileo <dileob23@gmail.com>
2025-06-13 08:52:05 -0400

    Implement basic jest test for GET comics route

--- backend/tests/comics.test.js
+++ backend/tests/comics.test.js
@@ -1,12 +1,27 @@
 
 // comics.test.js
 
+const request = require('supertest');
+const testComicId = "684a294f50c68e07bf7bcab6";
 
-const app = require('../app');
+// GET tests
+describe('GET /comics', () => {
+    // Basic GET
+    test('should return a list of comics', async () => {
+        const response = await request('http://localhost:5000').get('/comics');
 
-// Basic GET test
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
 
-// Basic GET by id test
+        if (response.body.length > 0) {
+            expect(response.body[0]).toHaveProperty('title');
+        }
+    });
+
+    // GET by id
+    test('should return a single comic by id', async () => {
+    });
+});
 
 // Basic POST test
 

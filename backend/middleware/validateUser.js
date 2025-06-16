5f6384d0e302888c1e610788467f25c2d82989fd
Brendan Dileo <dileob23@gmail.com>
2025-06-16 19:10:58 -0400

    Add username regex for user validator

--- backend/middleware/validateUser.js
+++ backend/middleware/validateUser.js
@@ -4,6 +4,9 @@
 // Brendan Dileo, June 2025
 
 const logger = require('../utils/logger');
+const usernameRegex = /^[a-zA-Z0-9_]{3,30}$/;
+const emailRegex = //;
+const passwordRegex = //;
 
 const validateUser = (req, res, next) => {
     const { username, email, password } = req.body;
@@ -11,6 +14,17 @@ const validateUser = (req, res, next) => {
         logger.error('One of the required fields are missing!');
         return res.status(400).json({ message: 'Username, email, and password are required.' });
     }
+
+    // Validate username against regex
+    if (!usernameRegex.test(username)) {
+        logger.error(`Invalid username: ${username}`);
+        return res.status(400).json({ message: 'Username must be 3-30 characters and only contain letters, numbers, or underscores.' });
+    }
+
+    // Validate email against regex
+    
+    // Validate password against regex
+
     next();
 };
 

546163e3fc6cd8b825164caa59fd06996f75011b
Brendan Dileo <dileob23@gmail.com>
2025-06-17 18:28:37 -0400

    Delete unused frontend

---
+++ backend/middleware/validateShop.js
@@ -0,0 +1,12 @@
+
+// validateShop.js
+// Validates incoming shop related data
+// Brendan Dileo, June 2025
+
+const logger = require('../utils/logger');
+const validator = require('validator');
+
+const validateShop = (req, res, next) => {
+}
+
+module.exports = validateShop;

0180748aff29db70484d71c1f23b87f7d4b81902
Brendan Dileo <dileob23@gmail.com>
2025-06-18 18:43:30 -0400

    Add PUT route to update a shop by id

--- backend/routes/shopRoutes.js
+++ backend/routes/shopRoutes.js
@@ -49,8 +49,28 @@ router.post('/', validateShop, async (req, res) => {
 
 // PUT to update a shop by id
 router.put('/:id', async (req, res) => {
+    try {
+        const updatedShop = await Shop.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedShop) {
+            logger.error(`Cannot find the Shop! ${error.message}`);
+            return res.status(404).json({ message: 'Shop not found!' });
+        }
+        
+        logger.log(`Shop Updated Successfully!`);
+        return res.status(200).json({ message: 'Shop updated successfully!', shop: updatedShop });
+    } catch (error) {
+        logger.error(`An error occurred while updating the Shop: ${error.message}`);
+        res.status(500).json({ message: 'Server Error!' });
+    }
 });
 
 // DELETE to delete a shop by id
 router.delete('/:id', async (req, res) => {
 });
+
+module.exports = router;

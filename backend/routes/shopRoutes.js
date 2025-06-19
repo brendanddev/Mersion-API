bf543c4ab95ed5d81415ddda12d1361141652285
Brendan Dileo <dileob23@gmail.com>
2025-06-18 21:16:21 -0400

    Implement GET search for shops route

--- backend/routes/shopRoutes.js
+++ backend/routes/shopRoutes.js
@@ -23,6 +23,23 @@ router.get('/', async (req, res) => {
     }
 });
 
+// GET to search for shops
+router.get('/search', async (req, res) => {
+    const { name, tag } = req.query;
+    const filter = {};
+
+    if (name) filter.name = new RegExp(name, 'i');
+    if (tags) filter.tags = tag;
+
+    try {
+        const shops = await Shop.find(filter);
+        return res.status(200).json({ shops: shops });
+    } catch (error) {
+        logger.error(`An error occurred while searching for the shops: ${error.message}`);
+        res.status(500).json({ message: 'Server Error!' });
+    }
+});
+
 // POST to add a new shop
 router.post('/', validateShop, async (req, res) => {
     const { name, address, email, phone, website, notes, tags } = req.body;

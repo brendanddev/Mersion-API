d532c8635f619272fdf806250af6b75970f4d256
Brendan Dileo <dileob23@gmail.com>
2025-06-19 18:23:39 -0400

    Setup maven project

---
+++ kollectbin-cli/src/test/java/com/brendanddev/kollectbin/AppTest.java
@@ -0,0 +1,17 @@
+package com.brendanddev.kollectbin;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+    
+}

7421a11239ff31369cc5751c33bfbbbdd0405665
Brendan Dileo <dileob23@gmail.com>
2025-06-19 20:18:11 -0400

    Update to new jackson version

--- kollectbin-cli/src/main/java/com/brendanddev/kollectbin/ObjectToJson.java
+++ kollectbin-cli/src/main/java/com/brendanddev/kollectbin/ObjectToJson.java
@@ -1,7 +1,15 @@
 package com.brendanddev.kollectbin;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ObjectToJson {
+
+    public static String convertObject(Comic comic) {
+        ObjectMapper objMapper = new ObjectMapper();
+        try {
+            
+        }
+
+    }
     
 }

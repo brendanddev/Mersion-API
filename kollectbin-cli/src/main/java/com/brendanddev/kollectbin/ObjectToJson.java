f5c8e3a0eb4e3c63f1f8d377abde60e0a55a1267
Brendan Dileo <dileob23@gmail.com>
2025-06-19 20:28:22 -0400

    Add object mapper conversion

--- kollectbin-cli/src/main/java/com/brendanddev/kollectbin/ObjectToJson.java
+++ kollectbin-cli/src/main/java/com/brendanddev/kollectbin/ObjectToJson.java
@@ -7,9 +7,11 @@ public class ObjectToJson {
     public static String convertObject(Comic comic) {
         ObjectMapper objMapper = new ObjectMapper();
         try {
-            
+            return objMapper.writeValueAsString(comic);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-
     }
     
 }

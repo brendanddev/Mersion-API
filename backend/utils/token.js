d5ad8b0b8d8e0295ce41a03cfae388f3471d1b45
Brendan Dileo <dileob23@gmail.com>
2025-06-23 09:44:30 -0400

    Update token logic to generate a refresh token

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -5,8 +5,8 @@
 
 const jwt = require('jsonwebtoken');
 
-// Generates a jwt token for a user
-const generateToken = (user) => {
+// Generates and signs the jwt access token for a user
+const generateAccessToken = (user) => {
     // Payload data to encode inside token
     const payload = {
         userId: user._id,
@@ -16,11 +16,31 @@ const generateToken = (user) => {
     // Sign the token with secret
     const token = jwt.sign(
         payload,
-        process.env.JWT_SECRET,
+        process.env.ACCESS_TOKEN_SECRET,
         { expiresIn: '1h' }
     );
 
     return token;
 }
 
-module.exports = generateToken;
+// Generates and signs the jwt refresh token
+const generateRefreshToken = (user) => {
+
+    const payload = { userId: user._id }
+
+    return jwt.sign(
+        payload,
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '2h' }
+    );
+}
+
+// Sends the access token to the client
+const sendAccessToken = (req, res, accessToken) => {
+}
+
+// Sends the refresh token to the client
+const sendRefreshToken = (req, res, accessToken) => {
+}
+
+module.exports = generateAccessToken;

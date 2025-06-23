87b8849561517f19e9ffa3a7641355bde03db8d1
Brendan Dileo <dileob23@gmail.com>
2025-06-23 09:48:01 -0400

    Update user model to store tokens

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     verified: { type: Boolean, default: false },
+    refreshToken: String,
     createdAt: { type: Date, default: Date.now() }
 });
 
